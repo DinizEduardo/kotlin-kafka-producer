3aa9fa4250c404e0bc7f0322ea9b2400461b378a
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-14 21:48:25 -0300

    feat: initial commit

---
+++ src/test/kotlin/br/com/kotlin/example/producer/ProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.kotlin.example.producer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
